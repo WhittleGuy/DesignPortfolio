eb3066c33943d0bcf1b2e8a53278d3acb9a241e2
Brandon <bwwmb6@mst.edu>
2020-01-06 06:57:56 -0600

    projects preliminaries

--- src/js/pages/projects.js
+++ src/js/pages/projects.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
+import Project from "../components/project";
+import Salty from "../../images/salty-example.jpg";
+import Ps from "../../images/ps-example.jpg";
+import Rmv from "../../images/RMV-example.jpg";
 
 class Projects extends Component {
   render() {
-    return <div className="projects-container"></div>;
+    return (
+      <div className="projects-container">
+        <Project source={Rmv} Name="30in30 Campaign" Company="Rank MI Vote" />
+        <Project
+          source={Ps}
+          Name="Reference Website"
+          Company="IT Media Services"
+        />
+        <Project source={Salty} Name="Stream Cards" Company="SaltyxB1tch" />
+      </div>
+    );
   }
 }
 
