e10d0c20ca1047df0220d7172d8fc487e6c96c00
Brandon <bwwmb6@mst.edu>
2020-01-08 06:37:41 -0600

    Grammatical correction

--- src/js/components/twitter.js
+++ src/js/components/twitter.js
@@ -8,7 +8,7 @@ class TwitterTag extends Component {
           <a
             className="twitter"
             href={"https://twitter.com/" + this.props.user}
-            target="_balnk"
+            target="_blank"
             rel="noopener noreferrer"
           >
             <i class="fab fa-twitter"></i>
