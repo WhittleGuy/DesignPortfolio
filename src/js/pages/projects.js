166e941135740a545b8a260dc529461e35769dbb
Brandon <bwwmb6@mst.edu>
2020-01-07 18:33:52 -0600

    Sass Overhaul | Project Additions

--- src/js/pages/projects.js
+++ src/js/pages/projects.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
 import Project from "../components/project";
 
-import Artboard1 from "../../images/Projects/100ppi/Artboard1.png";
-import Artboard2 from "../../images/Projects/100ppi/Artboard2.png";
-import Artboard3 from "../../images/Projects/100ppi/Artboard3.png";
-import Artboard4 from "../../images/Projects/100ppi/Artboard4.png";
-import Artboard5 from "../../images/Projects/100ppi/Artboard5.png";
-import Artboard6 from "../../images/Projects/100ppi/Artboard6.png";
-import Artboard7 from "../../images/Projects/100ppi/Artboard7.png";
-import Artboard8 from "../../images/Projects/100ppi/Artboard8.png";
-import Artboard9 from "../../images/Projects/100ppi/Artboard9.png";
-import Artboard10 from "../../images/Projects/100ppi/Artboard10.png";
-import Artboard11 from "../../images/Projects/100ppi/Artboard11.png";
-import Artboard12 from "../../images/Projects/100ppi/Artboard12.png";
-import Artboard13 from "../../images/Projects/100ppi/Artboard13.png";
-import Artboard14 from "../../images/Projects/100ppi/Artboard14.png";
-import Artboard15 from "../../images/Projects/100ppi/Artboard15.png";
+import RMVLogo from "../../images/Projects/100ppi/RMV/RMVLogo.png";
+import Graphremake from "../../images/Projects/100ppi/RMV/graphremake.png";
+import RMVFlyer from "../../images/Projects/100ppi/RMV/RMVFlyer.png";
+import RMVPostcard from "../../images/Projects/100ppi/RMV/RMVPostcard.png";
+import RMVwordmark from "../../images/Projects/100ppi/RMV/RMVwordmark.png";
+import Twitter1 from "../../images/Projects/100ppi/RMV/Twitter1.png";
+import Twitter2 from "../../images/Projects/100ppi/RMV/Twitter2.png";
+import Twitter3 from "../../images/Projects/100ppi/RMV/Twitter3.png";
+
+import SaltySS from "../../images/Projects/100ppi/Salty/saltyss.png";
+import SaltyBRB from "../../images/Projects/100ppi/Salty/saltybrb.png";
+import SaltyEndCard from "../../images/Projects/100ppi/Salty/saltyendcard.png";
+import SaltyHeader from "../../images/Projects/100ppi/Salty/saltyheader.png";
+import SaltyTFW from "../../images/Projects/100ppi/Salty/saltytfw.png";
+import SaltyYT from "../../images/Projects/100ppi/Salty/saltyyt.png";
+
+import Anbu from "../../images/Projects/100ppi/Misc Twitch/Anbu.png";
+import Nuck from "../../images/Projects/100ppi/Misc Twitch/Nuck_Logo_New.png";
+import Cam from "../../images/Projects/100ppi/Misc Twitch/twitch.png";
+
+import Crowder from "../../images/Projects/100ppi/Personal/crowder.png";
+import Eye from "../../images/Projects/100ppi/Personal/eye.png";
+import Moon from "../../images/Projects/100ppi/Personal/Moon.png";
+import Record from "../../images/Projects/100ppi/Personal/Record.png";
+
 import Spirograph from "../../images/spirograph.svg";
 import Logo from "../../images/BW_Logo.svg";
-import Sines from "../../images/Sines.svg";
 import Penrose from "../../images/Penrose.svg";
 
 class Projects extends Component {
@@ -46,35 +55,48 @@ class Projects extends Component {
             <div className="project">
               <h1>Rank MI Vote</h1>
               <div className="tiles">
-                <Project source={Artboard8} />
-                <Project source={Artboard4} />
-                <Project source={Artboard3} />
-                <Project source={Artboard5} />
-                <Project source={Artboard10} />
-                <Project source={Artboard9} />
-                <Project source={Artboard1} />
-                <Project source={Artboard2} />
+                <Project source={RMVLogo} />
+                <Project source={RMVPostcard} />
+                <Project source={RMVwordmark} />
+                <Project source={Twitter1} />
+                <Project source={Twitter2} />
+                <Project source={Twitter3} />
+                <Project source={Graphremake} />
+                <Project source={RMVFlyer} />
               </div>
             </div>
+
             <div className="project">
               <h1>SaltyxB1tch</h1>
               <div className="tiles">
-                <Project source={Artboard13} />
-                <Project source={Artboard14} />
-                <Project source={Artboard15} />
-                <Project source={Artboard12} />
+                <Project source={SaltySS} />
+                <Project source={SaltyBRB} />
+                <Project source={SaltyTFW} />
+                <Project source={SaltyHeader} />
+                <Project source={SaltyYT} />
+                <Project source={SaltyEndCard} />
               </div>
             </div>
+
+            <div className="project">
+              <h1>Miscellaneous Twitch</h1>
+              <div className="tiles">
+                <Project source={Anbu} />
+                <Project source={Nuck} />
+                <Project source={Cam} />
+              </div>
+            </div>
+
             <div className="project">
               <h1>Personal</h1>
               <div className="tiles">
-                <Project source={Artboard6} />
-                <Project source={Artboard7} />
-                <Project source={Artboard11} />
-                <Project source={Spirograph} />
+                <Project source={Crowder} />
+                <Project source={Moon} />
+                <Project source={Eye} />
+                <Project source={Record} />
                 <Project source={Logo} />
+                <Project source={Spirograph} />
                 <Project source={Penrose} />
-                <Project source={Sines} />
               </div>
             </div>
           </div>
